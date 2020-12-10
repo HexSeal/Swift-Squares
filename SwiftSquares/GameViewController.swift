f69c0e23341effd1cec00606170e2409187aec0d
HexSeal <maxfin13@gmail.com>
2020-12-09 20:26:07 -0500

    [add] Timer and last score on the homescreen, as well as automatically exiting when game is over

--- SwiftSquares/GameViewController.swift
+++ SwiftSquares/GameViewController.swift
@@ -7,38 +7,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    let buttons = ["1", "2", "3", "4", "5", "6", "7", "8"]
+class GameViewController: UIViewController {
+    let buttons = [0, 1, 2, 3, 4, 5, 6, 7]
+    let time = 60
+    var buttonsArray: [UIButton] = []
     var score: Int = 0
-    var currentButton: String = "0"
+    var currentButton: Int = 0
+    var randomIndex: Int!
+    var lastScore: Int?
+    var startViewController: StartViewController?
+    
+    
     
     @IBOutlet weak var backPressed: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    @IBOutlet weak var btn0: UIButton!
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    
     
     @IBAction func tilePressed(_ sender: UIButton) {
-        let buttonTitle = sender.titleLabel?.text
-        if buttonTitle == currentButton {
+        print(sender.tag)
+        print(currentButton)
+        if sender.tag == currentButton {
             score += 1
             scoreLabel.text = "Score: \(score)"
         }
+        buttonsArray[currentButton].backgroundColor = #colorLiteral(red: 1, green: 0.2156862745, blue: 0.3725490196, alpha: 1)
+        setCurrentButton()
     }
     
-    func activateButton() {
-        let randomIndex = Int.random(in: 0..<buttons.count)
-        for case let button as UIButton in self.view.subviews {
-            if button == randomIndex {
-                
+    func setCurrentButton() {
+        randomIndex = Int.random(in: 0..<buttons.count)
+        currentButton = randomIndex
+        buttonsArray[randomIndex].backgroundColor = UIColor.blue
+    }
+    
+    func createTimer() {
+        var runCount = 0
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {
+            timer in
+            runCount += 1
+            self.timeLabel.text = "Time left: \(self.time-runCount)"
+            if runCount == self.time {
+                timer.invalidate()
+                self.lastScore = self.score
+                self.gameOverReset()
             }
         }
+    }
+    
+    func gameOverReset(){
+        startViewController?.onTimerExpire(data: lastScore!)
+        startViewController?.navigationController?.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        buttonsArray = [btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7]
+        setCurrentButton()
+        createTimer()
         
-
     }
-
-
 }
-
--- SwiftSquares/StartViewController.swift
+++ SwiftSquares/StartViewController.swift
@@ -8,22 +8,35 @@
 import UIKit
 
 class StartViewController: UIViewController {
-
+    @IBOutlet weak var lastScoreLabel: UILabel!
+    @IBAction func unwindToStart(segue:UIStoryboardSegue) {
+        
+    }
+    
+    
+    func onTimerExpire(data: Int) {
+        lastScoreLabel.text = "Last Score: \(data)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let vc = segue.destination as? GameViewController
+        vc?.startViewController = self
+
     }
-    */
+    
 
 }
